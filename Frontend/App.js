cf71d0fb3de12256ace2d5300f078c2e0be4bbb5
Nomuulin <nomuulin@letumongolia.com>
2023-10-23 15:59:03 -0500

    hey

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("App executed");
+  
   return (
     <View style={styles.container}>
       <Text>CountEZ</Text>
