7b59a238a2b3c30bb28105a3c48485aa3eff2ea8
NaodP <nalf4459@gmail.com>
2023-10-30 00:42:15 -0500

    add get total endpoint

--- Backend/routes/rounds/Rounds.js
+++ Backend/routes/rounds/Rounds.js
@@ -5,7 +5,18 @@ const cors = require('cors');
 router.use(cors());
 router.use(express.json());
 
+const GetTotal = require("../../functions/GetTotal.js")
 const InsertRound = require("../../functions/InsertRound.js");
+
+router.get("/", async (req, res) => {
+	try {
+		const response = await GetTotal();
+		res.status(200).json(response);
+
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
 
 router.post("/", async (req, res) => {
 	try {
@@ -14,7 +25,7 @@ router.post("/", async (req, res) => {
 		const response = await InsertRound({ userID, data, startTime, endTime, date });
 
 		res.status(200).json(response);
-
+		
 	} catch (error) {
 		res.status(500).json(error);
 	}
